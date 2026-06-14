54ab63d5c95cc5c21e6c27c314d276db0698788f
Sid <siddheshsonawane316@gmail.com>
2026-06-14 23:26:00 +0530

    My First commit

---
+++ src/JAVA/Practice/practice.java
@@ -0,0 +1,14 @@
+package JAVA.Practice;
+
+public class practice{
+     static void main(String[] args) {
+       Operaation2 Addition=(a, b) -> a+b;
+       Operaation2 Substraction=(a, b) -> a-b;
+
+        System.out.println(Addition.operate(5,10));
+         System.out.println(Substraction.operate(5,10));
+    }
+
+
+
+}
