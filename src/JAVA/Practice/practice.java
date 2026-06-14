4183ef34c2f567f0f43c330980fcee3b774d1b4f
Sid <siddheshsonawane316@gmail.com>
2026-06-14 23:45:36 +0530

    My Third Commit

--- src/JAVA/Practice/practice.java
+++ src/JAVA/Practice/practice.java
@@ -4,7 +4,7 @@ public class practice{
      static void main(String[] args) {
        Operaation2 Addition=(a, b) -> a+b;
        Operaation2 Substraction=(a, b) -> a-b;
-
+         System.out.println("********************");
         System.out.println(Addition.operate(5,10));
          System.out.println(Substraction.operate(5,10));
          System.out.println("********************");
