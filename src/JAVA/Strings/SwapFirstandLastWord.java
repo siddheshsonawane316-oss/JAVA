e677f0b23c8fecfb3f1e58f9b7fde553de72b1c1
Sid <siddheshsonawane316@gmail.com>
2026-06-29 16:26:45 +0530

    Swap First and Last Word

--- src/JAVA/Strings/SwapFirstandLastWord.java
+++ src/JAVA/Strings/SwapFirstandLastWord.java
@@ -1,4 +1,14 @@
 package JAVA.Strings;
 
-public class SwapFirstandLast {
+
+
+public class SwapFirstandLastWord {
+    static void main() {
+  StringBuilder str=new StringBuilder("this is the new java String");
+  String FirstWord=str.substring(0,str.indexOf(" "));
+  String LastWord=str.substring(str.lastIndexOf(" ")+1,str.length());
+    str.replace(0,str.indexOf(" "),LastWord);
+    str.replace(str.lastIndexOf(" ")+1,str.length(),FirstWord);
+        System.out.println(str);
+    }
 }
