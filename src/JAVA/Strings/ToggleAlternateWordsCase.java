caaa600b797577bdb708b94c4db9a4abf1943dec
Sid <siddheshsonawane316@gmail.com>
2026-07-01 09:03:33 +0530

    Toggle case of alternate words in a sentence

--- src/JAVA/Strings/ToggleAlternateWordsCase.java
+++ src/JAVA/Strings/ToggleAlternateWordsCase.java
@@ -1,16 +1,41 @@
 package JAVA.Strings;
 
-public class ToggleAlternateCase {
+public class ToggleAlternateWordsCase {
     static void main() {
-     StringBuilder str=new StringBuilder("SSsjSJsS");
-     for (int i=0;i<str.length();i++){
-         if(i%2==0){
-             str.setCharAt(i,Character.toUpperCase(str.charAt(i)));
-         }else{
-             str.setCharAt(i,Character.toLowerCase(str.charAt(i)));
-         }
+        StringBuilder str = new StringBuilder("Hello JAVA is FUN");
 
-     }
+        // Marks the starting index of the current word
+        int start=0;
+
+        // false -> convert current word to lowercase
+        // true  -> convert current word to uppercase
+        boolean toggle=false;
+
+        // Traverse the sentence.
+        // i <= str.length() is used so that the last word
+        // is processed even though there is no space after it.
+        for (int i = 0; i <=str.length(); i++) {
+
+            // 'i == str.length()' must be checked first.
+            // Due to short-circuit evaluation, Java will not
+            // execute str.charAt(i) when i == str.length().
+            if(i==str.length() || str.charAt(i)==' '){
+
+                // Ensure the word is not empty
+                if(start<i ){
+                    if(toggle==false){
+                        str.replace(start,i,str.substring(start,i).toLowerCase());
+                        toggle=true;
+                    }
+                    else if(toggle==true){
+                        str.replace(start,i,str.substring(start,i).toUpperCase());
+                        toggle=false;
+                    }
+                }
+                // Move start to the beginning of the next word
+                start=i+1;
+            }
+        }
         System.out.println(str);
     }
 }
