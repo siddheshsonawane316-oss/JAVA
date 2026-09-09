6a03173a7c66690adfefa6759c5d91c698ff1c11
Sid <siddheshsonawane316@gmail.com>
2026-09-09 07:36:31 +0530

    Search in Rotated Sorted Array

--- src/JAVA/Arrays/Basics/SearchinRotatedSortedArray.java
+++ src/JAVA/Arrays/Basics/SearchinRotatedSortedArray.java
@@ -1,6 +1,6 @@
 package JAVA.Arrays.Basics;
 
-    public class SearchinRotatedSortedArray {
+public class SearchinRotatedSortedArray {
     static void main() {
         int nums[] = {4, 5, 6, 7, 0, 1, 2};
         int target = 0;
